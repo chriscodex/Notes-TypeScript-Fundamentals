3d625ad4f923cf39c4b68c18602259481691a7ed
Christian <gonzalo.espinozacadillo@gmail.com>
2022-09-28 16:00:25 -0500

    examples to not possible added

--- src/06-strings.ts
+++ src/06-strings.ts
@@ -1,4 +1,8 @@
 (()=>{
     // Inflicted by ts
     let productTitle = 'this is my product';
+    /* Not possible */
+    // productTitle = null;
+    // productTitle = ()=>{};
+    // productTitle = 123
 })()
