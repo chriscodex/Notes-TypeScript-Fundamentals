915448d67d742e96e073f50a6c432ee6599697f1
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-03 21:25:27 -0500

    function to test ts deduction in union types created

--- src/09-union-types.ts
+++ src/09-union-types.ts
@@ -3,4 +3,13 @@
     let myDinamicVar : number | string;
     myDinamicVar = 121;
     myDinamicVar = 'hello';
+
+    // deduction of data type
+    function greeting(myText : number | string) {
+        if (typeof myText === 'string') {
+            console.log(myText.toLowerCase())
+        } else {
+            console.log(myText.toFixed(1))
+        }
+    }
 })();
