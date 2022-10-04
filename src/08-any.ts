4e9112b4e7548f20682a90cd5fad52fb5277880b
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-03 19:07:33 -0500

    rta2  printed in console

--- src/08-any.ts
+++ src/08-any.ts
@@ -7,8 +7,9 @@
     // Casting any
     myDinamicVar = 'Hola';
     const rta = (myDinamicVar as string).toLowerCase();
-    console.log(rta)
+    console.log(rta);
 
     // Other way
     const rta2 = (<number>myDinamicVar).toFixed();
+    console.log(rta2);
 })()
