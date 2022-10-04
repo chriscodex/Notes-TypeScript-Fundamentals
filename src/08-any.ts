5803e5b55e07df56dfee81040eca93caf6d5b74e
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-03 19:05:52 -0500

    dynamic var casted to string

--- src/08-any.ts
+++ src/08-any.ts
@@ -3,4 +3,8 @@
     myDinamicVar = 'hello';
     myDinamicVar = 123;
     myDinamicVar = false;
+    
+    // Casting any
+    myDinamicVar = 'Hola';
+    const rta = (myDinamicVar as string).toLowerCase();
 })()
