c9c9463da9de9098592c0883183325583cdd1d5b
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-05 14:03:48 -0500

    undefined and null declared

--- src/11-null-undefined.ts
+++ src/11-null-undefined.ts
@@ -2,4 +2,8 @@
     // Not possible
     //let myNumber: number = undefined;
     //let muString: string = null;
+
+    // Ways to declare null and undefined
+    let myNull : null = null;
+    let myUndefined: undefined = undefined;
 })()
