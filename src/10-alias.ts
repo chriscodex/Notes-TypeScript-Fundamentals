dec41a380002e0ee5b272b59f9bdf743d28cfcea
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-04 22:49:38 -0500

    function using alias created

--- src/10-alias.ts
+++ src/10-alias.ts
@@ -1,4 +1,11 @@
 (()=>{
+    // Alias type
     type UserID = string | number
     let userId : UserID
+
+    function foo(userId: UserID) {
+        if (typeof userId === 'string') {
+            console.log(`string ${userId}`)
+        }
+    }
 })()
