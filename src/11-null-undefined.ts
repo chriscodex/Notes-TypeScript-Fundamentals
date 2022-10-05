4afe9337a35faee1e905eaeb35e99dc98e89cd20
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-05 14:05:26 -0500

    number variable changed to value

--- src/11-null-undefined.ts
+++ src/11-null-undefined.ts
@@ -6,4 +6,10 @@
     // Ways to declare null and undefined
     let myNull : null = null;
     let myUndefined: undefined = undefined;
+
+    // Cases to use
+    let myNumber: number | null = null;
+    myNumber = 111;
+
+    
 })()
