74794dcc4b76627f34f3a1baee03ce8ab73eddc3
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-07 15:19:55 -0500

    function v2 created

--- src/12-functions.ts
+++ src/12-functions.ts
@@ -1,5 +1,7 @@
 (()=>{
     type Sizes = 'S' | 'M' | 'L' | 'XL';
+
+    // 1st Version
     function createProductToJson(
         title: string,
         createdAt: Date,
@@ -17,4 +19,19 @@
     const product1 = createProductToJson('P1', new Date(), 12, 'S')
     console.log(product1)
     console.log(product1.title)
+
+    // 2nd Version
+    const createProductToJsonV2 = (
+        title: string,
+        createdAt: Date,
+        stock: number,
+        size?: Sizes
+    ) => {
+        return {
+            title,
+            createdAt,
+            stock,
+            size
+        }
+    }
 })();
