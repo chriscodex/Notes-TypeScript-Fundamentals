008f6aec7624327640fa819df33d7ac93af4684b
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-08 15:59:48 -0500

    function return changed from number to string

--- src/13-return-functions.ts
+++ src/13-return-functions.ts
@@ -1,9 +1,9 @@
 (() => {
-    const calcTotal = (prices: number[]) => {
+    const calcTotal = (prices: number[]):string => {
         let total = 0;
         prices.forEach((item)=>{
             total += item;
         });
-        return total;
+        return total.toString();
     }
 })();
