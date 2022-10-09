f5e4bfd7fe424328bbdeb31b308888f044fdc912
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-09 16:35:20 -0500

    login v2 using objectos done

--- src/14-functions-objs.ts
+++ src/14-functions-objs.ts
@@ -1,11 +1,13 @@
 (()=>{
     // Classic way
     const login = (email: string, password: number) => {
+        console.log(email, password)
+    };
 
-    }
+    login('christ@christ', 1234);
 
     // Using objects
-    const loginV2 = () =>{
-
-    }
+    const loginV2 = (data: {email: string, password: number}) =>{
+        console.log(data.email, data.password);
+    };
 });
