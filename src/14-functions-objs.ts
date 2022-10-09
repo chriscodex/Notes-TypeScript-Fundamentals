1b281ccd1cdec795e93bcf1ad29c6fa0f371f3ef
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-09 16:38:27 -0500

    login v2 optional propperty added

--- src/14-functions-objs.ts
+++ src/14-functions-objs.ts
@@ -7,7 +7,12 @@
     login('christ@christ.co', 1234);
 
     // Using objects
-    const loginV2 = (data: {email: string, password: number}) =>{
+    const loginV2 = (data: {
+        email: string, 
+        password: number
+        // Optional
+        phone?: string
+    }) =>{
         console.log(data.email, data.password);
     };
 
