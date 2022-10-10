632e50d376d70debaf13c8c4f90a47892b94f101
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-10 17:19:23 -0500

    type Product created

--- src/15-objs.ts
+++ src/15-objs.ts
@@ -1,3 +1,40 @@
 (()=>{
-    
+    type Sizes = 'S' | 'M' | 'L' | 'XL'
+    type Product = {
+        title: string,
+        createdAt: Date,
+        stock: number,
+        size: Sizes
+    }
+
+    // Array created
+    const products: any[] = [];
+
+    const addProduct = (
+        data: {
+            title: string,
+            createdAt: Date,
+            stock: number,
+            size?: Sizes
+        }
+    ) => {
+        products.push(data)
+    }
+
+    // Add a product using an object as parameter without the optional property
+    addProduct({
+        title: 'P1',
+        createdAt: new Date(),
+        stock: 12,
+    })
+
+    // Add a product using and object as parameter with optional parameter
+    addProduct({
+        title: 'P2',
+        createdAt: new Date(),
+        stock: 7,
+        size: 'M'
+    })    
+
+    console.log(products);
 })();
