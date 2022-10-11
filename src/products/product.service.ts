9be1a3bbcb17f5d72bb82ea3185ff19edf3cc9f6
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-11 17:56:25 -0500

    total stock function initialized

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -6,5 +6,12 @@ export const products: Product[] = [];
 // Add product function
 export const addProduct = (data: Product
 ) => {
-    products.push(data)
+    products.push(data);
 }
+
+const totalStock = () => {
+    products.forEach((item)=>{
+        let total = 0;
+
+    });
+};
