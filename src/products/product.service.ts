df0ec965c5b24d0561ab2eae775c91803ebb4b37
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-11 17:51:17 -0500

    function added

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,1 +1,10 @@
 import {Product} from './products.model'
+
+// Array created
+const products: Product[] = [];
+
+// Add product function
+const addProduct = (data: Product
+) => {
+    products.push(data)
+}
