323c96da2594ef0ca77a04134e7a3ed3ec5bd72e
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-12 19:23:22 -0500

    comments added

--- src/16-date-fns.ts
+++ src/16-date-fns.ts
@@ -2,5 +2,9 @@ import { subDays, format } from 'date-fns';
 
 const date = new Date(1998, 1, 28); // Feb 28 - 1998
 
+// subtract 30 days
 const rta = subDays(date, 30);
+console.log(rta);
+
+// formatting the date
 const str = format(rta, 'yyyy/MM/dd');
