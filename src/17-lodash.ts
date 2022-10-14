8efc8aeb0e537766e07308b36ea60cf046f540dc
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-13 19:44:45 -0500

    array of data created

--- src/17-lodash.ts
+++ src/17-lodash.ts
@@ -1,5 +1,24 @@
 import _ from 'lodash';
 
-/*Ways
+/*Official documentation
 var _ = require('lodash)
 */
+
+const data = [
+    {
+        username: 'christian',
+        role: 'admin'
+    },
+    {
+        username: 'diego',
+        role: 'seller'
+    },
+    {
+        username: 'eduardo',
+        role: 'seller'
+    },
+    {
+        username: 'santiago',
+        role: 'customer'
+    }
+]
