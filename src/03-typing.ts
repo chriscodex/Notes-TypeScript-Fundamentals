01c3c6f7adc91a52e05ae6b2198b29a351085815
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-21 12:40:58 -0500

    typing concepts added

---
+++ src/03-typing.ts
@@ -0,0 +1,9 @@
+(() => {
+    // Detección automática de tipo
+    let myProductName = "Product 1"
+    let myProductPrice = 400
+
+    // Asignar otro valor
+    myProductName = "Product 2"
+    myProductPrice = 1000
+})()
