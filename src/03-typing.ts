b0f3a49767451b23010efd6e1e90474a6ca4408a
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-21 12:43:59 -0500

    constant values added

--- src/03-typing.ts
+++ src/03-typing.ts
@@ -4,6 +4,16 @@
     let myProductPrice = 400
 
     // Asignar otro valor
-    myProductName = "Product 2"
+    myProductName = "Product Name"
     myProductPrice = 1000
+
+    // Metodos de cada tipo específico de tipo
+    myProductName.toLowerCase()
+    myProductPrice.toFixed()
+
+    // Valores constantes
+    const myProductStock = 10
+    const myProductName2 = "Product 2"
 })()
+
+
